dec2a01e859d9ffa290ca1946c6d4fa6295a869a
Thu Tran <tntran@smith.edu>
2022-04-12 16:35:58 -0400

    Initial version of number guessing app

---
+++ app/src/main/java/com/numberguessinggame/MainActivity.java
@@ -0,0 +1,63 @@
+package com.numberguessinggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final int MAX_NUMBER = 100;
+    public static final Random RANDOM = new Random();
+    private TextView message;
+    private EditText number;
+    private Button play;
+    private int numberToFind, numberOfTries;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        message = (TextView) findViewById(R.id.txtMessage);
+        number = (EditText) findViewById(R.id.txtNumber);
+        play = (Button) findViewById(R.id.btnPlay);
+
+        play.setOnClickListener(this);
+        newGame();
+    }
+
+    public void onClick(View view) {
+        if (view == play) {
+            validatePlay();
+        }
+    } // end of onClick
+
+    private void newGame() {
+        numberToFind = RANDOM.nextInt(MAX_NUMBER) + 1;
+        message.setText("New guess!");
+        number.setText("");
+        numberOfTries = 0;
+    }
+
+    private void validatePlay() {
+        int num = Integer.parseInt(number.getText().toString()); // to convert input to integer
+        numberOfTries++;
+
+        if (num == numberToFind) {
+            Toast.makeText(this, "Congratulations! You got it in " + numberOfTries + " tries.", Toast.LENGTH_SHORT).show();
+            newGame();
+        } else if (num > numberToFind) {
+            message.setText("Too high!");
+        } else if (num < numberToFind) {
+            message.setText("Too low!");
+        }
+    }
+}
